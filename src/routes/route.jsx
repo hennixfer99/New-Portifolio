a223b0d7424a5011e744466b99a1a32ff8be8692
Hennixfer99 <Henriferreira99@gmail.com>
2025-05-14 12:17:05 -0300

    solved_vercel

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -3,10 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage/Homepage";
 import Projects from "../pages/Projects/Projects";
 import Qualifications from "../pages/Qualifications/Qualifications";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function Routes() {
     return (
         <>
+            <Route exact path="/">
+                <Redirect to="/sobre" />
+            </Route>
             <Switch>
                 <Route path="/sobre">
                     <Homepage />
